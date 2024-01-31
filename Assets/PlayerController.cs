1b6c183c6438177a023f1ba5d27c0d8201868fb8
JackyWuCoder <JackyWuCoder@gmail.com>
2024-01-30 17:39:37 -0800

    Player Controller (2)
    1) Implemented "Player" rotation that simulates first-person experience using the Main Camera object.
    2) Implemented a CheckGround function to see if the "Player" is on the ground.
    3) Implemented a CheckJump function to see if the "Player" pressed Space-key to jump, as well as making sure the "Player" cannot double jump by doing a GroundCheck before jumping again.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,25 +12,43 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool invertMouse;
     [SerializeField] private float sprintMultiplier = 2.0f;
 
+    [Header("Player Jump")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpVelocity;
+
     private CharacterController characterController;
     private float horizontalInput, verticalInput;
     private float mouseX, mouseY;
     private float moveMultiplier = 1.0f;
+    private float camXRotation;
+    private bool isGrounded;
+    private Vector3 playerVelocity;
+
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Hide Mouse
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         GetInput();
+        RotatePlayer();
+
+        GroundCheck();
         MovePlayer();
+        JumpCheck();
     }
 
     private void GetInput()
@@ -45,10 +63,46 @@ public class PlayerController : MonoBehaviour
     private void MovePlayer()
     {
         characterController.Move((transform.forward * verticalInput + transform.right * horizontalInput) * moveSpeed * moveMultiplier * Time.deltaTime);
+        //Ground Check
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        characterController.Move(playerVelocity * Time.deltaTime);
     }
 
-    private bool isSprint()
+    private void RotatePlayer()
     {
-        return (Input.GetKeyDown(KeyCode.LeftShift) == true) ? true : false;
+        //Player turn movement
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * mouseX);
+
+        //Camera up/down movement
+        camXRotation += Time.deltaTime * mouseY * turnSpeed * (invertMouse ? 1 : -1);
+        camXRotation = Mathf.Clamp(camXRotation, -50.0f, 50.0f); // Restricts up/down rotation to 50 degrees
+        cameraTransform.localRotation = Quaternion.Euler(camXRotation, 0, 0);
     }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
+    }
+
+    private void JumpCheck()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            playerVelocity.y = jumpVelocity;
+        }
+    }
+
+    // Used to check the visibility for collision using GroundCheck()
+    /*
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawSphere(groundCheck.position, groundCheckDistance);
+    }
+    */
 }
