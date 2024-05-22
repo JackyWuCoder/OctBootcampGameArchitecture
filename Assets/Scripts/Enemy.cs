404f3d853f15e67a84052716b40060168789d008
JackyWuCoder <JackyWuCoder@gmail.com>
2024-05-21 22:30:25 -0700

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,19 +5,45 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private Transform targetPoint;
+    [SerializeField] private Transform[] targetPoints;
 
+    [Range(0f, 0.5f)]
+    [SerializeField] private float accuracy = 0.1f;
+
+
+    [Range(0f, 5f)]
+    [SerializeField] private float waitTime = 1f;
+
+    private int currentTarget = 0;
     private NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.destination = targetPoints[currentTarget].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = targetPoint.position;
+        // Checking distance between enemy and target position
+        if (agent.remainingDistance < accuracy)
+        {
+            currentTarget++;
+            // Ensuring the current target does not exceed the number of target points
+            if (currentTarget >= targetPoints.Length)
+            {
+                currentTarget = 0;
+            }
+            StartCoroutine(PatrolAtPosition(waitTime));
+        }
+    }
+
+    private IEnumerator PatrolAtPosition(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        // Set new enemy position
+        agent.destination = targetPoints[currentTarget].position;
     }
 }
